904f0ab81e3e194abf45ac4dd919cb821ee8d686
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-01-24 21:10:39 -0300

    Adição dos modulos dashboard e courses

---
+++ src/app/shared/material/dashboard-material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatIconModule,
+    MatButtonModule
+  ]
+})
+export class DashboardMaterialModule { }
