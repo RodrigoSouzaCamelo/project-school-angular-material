905d93e0869ec568194bc0a10e2745c5fe58eb10
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-02-02 20:20:11 -0300

    Estilizando componente de lista de cursos

--- src/app/shared/material/courses-material.module.ts
+++ src/app/shared/material/courses-material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   exports: [
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule
   ]
 })
 export class CoursesMaterialModule { }
