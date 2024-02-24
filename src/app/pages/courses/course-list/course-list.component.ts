c2aa719de060502223b1819c36d38de33e63d75d
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-02-23 21:18:37 -0300

    Criação de categorias

--- src/app/pages/courses/course-list/course-list.component.ts
+++ src/app/pages/courses/course-list/course-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '@app/services/courses.service';
-import { Course } from '@app/shared/models/course';
+import { Category, Course } from '@app/shared/models/course';
 
 @Component({
   selector: 'app-course-list',
@@ -8,13 +9,23 @@ import { Course } from '@app/shared/models/course';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
+  private courseService = inject(CoursesService);
+  private fb = inject(FormBuilder)
 
   public courseList: Course[] = [];
+  categories = Object.values(Category);
+  form!: FormGroup;
 
-  constructor(private courseService: CoursesService) { }
+  constructor() { }
 
   ngOnInit(): void {
     this.getCourses();
+  }
+
+  public validation() {
+    this.form = this.fb.group({
+      category: ['']
+    })
   }
 
   public getCourses(): void {
--- src/app/shared/material/courses-material.module.ts
+++ src/app/shared/material/courses-material.module.ts
@@ -3,6 +3,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
@@ -10,7 +12,9 @@ import { MatDividerModule } from '@angular/material/divider';
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    MatSelectModule,
+    MatFormFieldModule
   ]
 })
 export class CoursesMaterialModule { }
