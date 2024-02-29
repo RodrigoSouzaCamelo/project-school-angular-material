f902b507103a617743de96797d2ee191eb7546be
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-02-29 20:31:51 -0300

    Layout paginação

--- src/app/shared/material/courses-material.module.ts
+++ src/app/shared/material/courses-material.module.ts
@@ -6,7 +6,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [],
@@ -19,6 +19,26 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatInputModule,
     MatFormFieldModule,
     MatPaginatorModule
+  ],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: customPaginator() }
   ]
 })
 export class CoursesMaterialModule { }
+
+function customPaginator() {
+  const customPaginatorIntl = new MatPaginatorIntl();
+  customPaginatorIntl.itemsPerPageLabel = '';
+  customPaginatorIntl.nextPageLabel = 'próxima';
+  customPaginatorIntl.previousPageLabel = 'anterior';
+  customPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0 || pageSize === 0) {
+      return `0 de ${length}`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return `${startIndex + 1} - ${endIndex} de ${length}`;
+  };
+  return customPaginatorIntl;
+}
