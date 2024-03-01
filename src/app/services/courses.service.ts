51ba405d4fb49e64ab1cc20c6ed365a41fa1de35
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-03-01 20:18:11 -0300

    Alteração do service de curso para paginação

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Course } from '@app/shared/models/course';
 import { Observable } from 'rxjs';
@@ -12,8 +12,14 @@ export class CoursesService {
   baseURL = `${environment.apiURL}/courses`;
   private http = inject(HttpClient);
 
-  public get(): Observable<Course[]> {
-    return this.http.get<Course[]>(`${this.baseURL}`);
+  public get(currentPage: number, pageSize: number, search: string, category: string): Observable<HttpResponse<any>> {
+    let url = `${this.baseURL}?_page=${currentPage}&_limit=${pageSize}`;
+    
+    if(search) url = `${url}&q=${search}`;
+    
+    if(category) url = `${url}&category=${category}`;
+
+    return this.http.get<any>(url, { observe: 'response' });
   }
 
   public getById(id: number): Observable<Course> {
