1e1e7390aad7a3ee465e59fb400a31882f1ce43e
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-03-11 18:50:56 -0300

    Lidando com erro no serviço de cursos

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Course } from '@app/shared/models/course';
-import { Observable } from 'rxjs';
+import { Observable, catchError, take, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -19,22 +19,60 @@ export class CoursesService {
     
     if(category) url = `${url}&category=${category}`;
 
-    return this.http.get<any>(url, { observe: 'response' });
+    return this.http
+      .get<any>(url, { observe: 'response' })
+      .pipe(
+        take(1),
+        catchError(this.handleError)
+      );
   }
 
   public getById(id: number): Observable<Course> {
-    return this.http.get<Course>(`${this.baseURL}/${id}`);
+    return this.http
+      .get<Course>(`${this.baseURL}/${id}`)
+      .pipe(
+        take(1),
+        catchError(this.handleError)
+      );
   }
 
   public post(course: Course): Observable<Course> {
-    return this.http.post<Course>(`${this.baseURL}`, course);
+    return this.http
+      .post<Course>(`${this.baseURL}`, course)
+      .pipe(
+        take(1),
+        catchError(this.handleError)
+      );
   }
 
   public put(course: Course): Observable<Course> {
-    return this.http.put<Course>(`${this.baseURL}/${course.id}`, course);
+    return this.http
+      .put<Course>(`${this.baseURL}/${course.id}`, course)
+      .pipe(
+        take(1),
+        catchError(this.handleError)
+      );
   }
 
   public delete(id: number): Observable<Course> {
-    return this.http.delete<Course>(`${this.baseURL}/${id}`);
+    return this.http
+      .delete<Course>(`${this.baseURL}/${id}`)
+      .pipe(
+        take(1),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    let errorMessage: string;
+
+    if(err.error instanceof ErrorEvent) 
+      errorMessage = `An error occurred: ${err.error.message}`;
+    else 
+      errorMessage = `Backend return code: ${err.status}: ${err.message}`;
+
+    console.error(err);
+
+    return throwError(() => errorMessage);
   }
 }
